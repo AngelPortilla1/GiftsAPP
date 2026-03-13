03225d322722326d7e3b69f4a4c629ad3edbf3b5
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-03-12 22:49:12 -0500

    New component called Trending-page

---
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-page',
+  imports: [],
+  template: `<p>trending-page works!</p>`,
+  styleUrl: './trending-page.css',
+
+})
+export class TrendingPage { }
