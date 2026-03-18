f64b710c1b0146f437df07992ba6500c6565e0e3
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-03-17 21:53:26 -0500

    Add folder with the enviroments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+production : true,
+companyName : 'Gifs',
+companyName2 : 'app',
+companySlogan : 'Find the best gifs in the world',
+
+
+//API KEYS
+
+// URLS
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+
+production : true,
+companyName : 'Gifs',
+companyName2 : 'app',
+companySlogan : 'Find the best gifs in the world',
+
+
+//API KEYS
+
+// URLS
+
+};
